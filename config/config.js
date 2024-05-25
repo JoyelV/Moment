8756da7ac6e28f5c75d58d78daf7a88364040c89
Joyel <Varghesejoyel71@gmail.com>
2024-05-25 12:21:42 +0530

    Add config file and modify userController

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const sessionSecret ="SECRET_SUPERKEY";
+
+const emailUser = "heatexservices@gmail.com";
+const emailPassword = 'dfuvuyzcbgavpptb';
+
+module.exports ={
+    sessionSecret,
+    emailUser,
+    emailPassword
+}
